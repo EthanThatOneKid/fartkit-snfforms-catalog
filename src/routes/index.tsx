676d5c1a183f80b066095de1b1e1d62f32637b81
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2025-09-05 23:21:28 -0700

    Show entire catalog by default

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,7 +5,7 @@ import { RedirectRoute } from "#/components/redirect.tsx";
 import { Catalog, CatalogScript } from "#/components/catalog.tsx";
 import type { CatalogItem } from "#/lib/snfforms.ts";
 import { searchCatalog } from "#/lib/orama.ts";
-import { findCatalogItem } from "#/lib/catalog.ts";
+import { catalogItems, findCatalogItem } from "#/lib/catalog.ts";
 
 export function IndexPageRoute() {
   return (
@@ -15,17 +15,18 @@ export function IndexPageRoute() {
         handler={async (ctx) => {
           const url = new URL(ctx.request.url);
           const search = url.searchParams.get("search");
-          const hits = search ? (await searchCatalog(search)).hits : [];
-          const items = hits.map((result) => {
-            const item = findCatalogItem(result.document.formId);
-            if (!item) {
-              throw new Error(
-                `Catalog item not found: ${result.document.formId}`,
-              );
-            }
+          const items = search
+            ? (await searchCatalog(search)).hits.map((result) => {
+              const item = findCatalogItem(result.document.formId);
+              if (!item) {
+                throw new Error(
+                  `Catalog item not found: ${result.document.formId}`,
+                );
+              }
 
-            return item;
-          });
+              return item;
+            })
+            : catalogItems;
 
           return new Response(
             <IndexPage search={search} items={items} />,
